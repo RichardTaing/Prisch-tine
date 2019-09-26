57f433fd6c6976ccc69dd9a3bad90a711b70d30c
Richard Taing <Richard.Taing@pexa.com.au>
2019-09-26 21:13:56 +1000

    Updated routing  for user login - to be cont...

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,29 +1,19 @@
 var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
-  // Load index page
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../public/html/main.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/login.html"));
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  app.get("/member", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/user.html"));
   });
 };
--- server.js
+++ server.js
@@ -1,16 +1,49 @@
 require("dotenv").config();
-var express = require("express");
-var exphbs = require("express-handlebars");
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-var db = require("./models");
+const db = require("./models");
+const usersRoute = require("./routes/userRoute");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Middleware
+// Login Authorisation ----------------------------------------------------
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+
+var authentication = function(req, res, next) {
+  //get the token from the header if present
+  const token = req.headers["x-access-token"] || req.headers["authorization"];
+  //if no token found, return response (without going to the next middelware)
+  if (!token) return res.status(401).send("Access denied. No token provided.");
+
+  try {
+    //if can verify the token, set req.user and pass to next middleware
+    const decoded = jwt.verify(token, config.get("myprivatekey"));
+    req.user = decoded;
+    next();
+  } catch (ex) {
+    //if invalid token
+    res.status(400).send("Invalid token.");
+  }
+};
+
+//use config module to get the privatekey, if no private key set, end the application
+if (!config.get("myprivatekey")) {
+  console.error("FATAL ERROR: myprivatekey is not defined.");
+  process.exit(1);
+}
+
+app.use(express.json());
+//use users route for api/users
+app.use("/api/users", usersRoute);
+
+// ---------------------------------------------------------------
 
 // Handlebars
 app.engine(
