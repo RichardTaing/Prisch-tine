44cf038a7601e0069955fcd8104f43acce16bd32
Richard Taing <Richard.Taing@pexa.com.au>
2019-09-26 23:10:34 +1000

    I HATE TRAVIS

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,32 +1,37 @@
 var auth = require("server");
+console.log(auth);
 var bcrypt = require("bcrypt");
 var User = require("../models/user").User;
 var validate = require("../models/user").validate;
 var express = require("express");
 var router = express.Router();
 
-router.get("/current", function (req, res) {
+router.get("/current", function(req, res) {
   var user = User.findById(req.user._id).select("-password");
   res.send(user);
 });
 
-router.post("/", function (req, res) {
+router.post("/", function(req, res) {
   // validate the request body first
   var _validate = validate(req.body);
-  
-  if (error) return res.status(400).send(error.details[0].message);
+  console.log(_validate);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
 
   //find an existing user
-  var user =  User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send("User already registered.");
+  var user = User.findOne({ email: req.body.email });
+  if (user) {
+    return res.status(400).send("User already registered.");
+  }
 
   user = new User({
     name: req.body.name,
     password: req.body.password,
     email: req.body.email
   });
-  user.password = await bcrypt.hash(user.password, 10);
-  await user.save();
+  user.password = bcrypt.hash(user.password, 10);
+  user.save();
 
   var token = user.generateAuthToken();
   res.header("x-auth-token", token).send({
